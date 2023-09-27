0d12401e226ed98f47221366245c0ed8d3770e57
marco perez <mp4667596@gmail.com>
2023-09-27 13:31:03 -0300

    avance

--- src/pages/PokeDexIdPage.jsx
+++ src/pages/PokeDexIdPage.jsx
@@ -13,65 +13,69 @@ const PokeDexIdPage = () => {
     useEffect(()=>{
         getPokemon()
     },[id])
-    console.log(pokemon)
+    
+    const typeColor = pokemon?.types[0].type.name;
 
 
   return (
-    <div>
-      <article className="pkecard__id">
-        <header>
+    <div className="pokecard__id__container">
+      <article className={`pkecard__id ${typeColor}-border`}>
+        <header className={`pokecard__id__header ${typeColor}-gradient`}>
         <img src={pokemon?.sprites.other["official-artwork"].front_default} alt="" />
         </header>
-        <span>#{pokemon?.order}</span>
-        <h2>{pokemon?.name}</h2>
-        <ul>
-          <li>Weight<span>{pokemon?.weight}</span></li>
-          <li>Height<span>{pokemon?.height}</span></li>
-        </ul>
-        <div>
-        <h3>types</h3>
-        <ul>
-          {
-            pokemon?.types.map(e => (
-              <li key={e.type.url}>
-                {
-                  e.type.name
-                }
-
-              </li>
-            ))
-          }
-        </ul>
-        </div>
-        <div>
-          <h3>abilities</h3>
-          <ul>
-            {
-              pokemon?.abilities.map(e => (
-                <li key={e.ability.url}>
+        <section className="pokecard__id__body">
+          <h4 className="pokecard__id__order">#{pokemon?.order}</h4>
+          <h2 className="pokecard__id__name">{pokemon?.name}</h2>
+          <ul className="pokecard__id__zise">
+            <li>Weight <br /><span>{pokemon?.weight}</span></li>
+            <li>Height <br /><span>{pokemon?.height}</span></li>
+          </ul>
+          <div className="pkecard__id__types__abilities">
+            <div>
+              <h3>types</h3>
+              <ul>
+              {
+                pokemon?.types.map(e => (
+                <li key={e.type.url} className={`pokecard__id__type ${typeColor}-background `}>
                   {
-                    e.ability.name
+                    e.type.name
                   }
 
                 </li>
-              ))
-            }
+                ))
+              }
           </ul>
-        </div>
-        <div>
-          <h2>states</h2>
-          <ul>
-            {
-              pokemon?.stats.map(e => (
-                <li key={e.stat.url}>
-                  <span>{e.stat.name}</span>
-                  <span>{e.base_stat}/150</span>
+          </div>
+          <div>
+          <h3>abilities</h3>
+            <ul>
+              {
+                pokemon?.abilities.map(e => (
+                  <li key={e.ability.url} className="pokecard__id__ability">
+                    {
+                      e.ability.name
+                    }
 
-                </li>
-              ))
-            }
-          </ul>
-        </div>  
+                  </li>
+                ))
+              }
+            </ul>
+            </div>
+            </div>
+          <div className="pokecard__id__states">
+            <h2>states</h2>
+            <ul>
+              {
+                pokemon?.stats.map(e => (
+                  <li key={e.stat.url}>
+                    <h4 className="pokecard__id_stat_name">{e.stat.name}</h4>
+                    <span className="pokecard__id_stat">{e.base_stat}/150</span>
+                  </li>
+                ))
+              }
+            </ul>
+          </div>
+        </section>
     </article>
     <article>
       <h3>Moves</h3>
